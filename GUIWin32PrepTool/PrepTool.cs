a833cde1c7b90d7216b66b98658fdd44ea43afd3
magik3r <64514975+magik3r@users.noreply.github.com>
2024-02-08 13:00:33 +0100

    Code cleanup

--- GUIWin32PrepTool/PrepTool.cs
+++ GUIWin32PrepTool/PrepTool.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Diagnostics;
-using System.IO;
 using System.Windows.Forms;
 
 namespace GUIWin32PrepTool
--- GUIWin32PrepTool/Program.cs
+++ GUIWin32PrepTool/Program.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using System.Security.Principal;
 using System.IO;
+using System.Security.Principal;
+using System.Windows.Forms;
 
 namespace GUIWin32PrepTool
 {
