f3ddb93f6284043d771335c62a10c9353eb2d79c
Noel <64514975+magik3r@users.noreply.github.com>
2024-02-13 09:50:15 +0100

    Removed icon

--- GUIWin32PrepTool/Form1.Designer.cs
+++ GUIWin32PrepTool/Form1.Designer.cs
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.textBox_SrcPath = new System.Windows.Forms.TextBox();
             this.button_SrcPath = new System.Windows.Forms.Button();
             this.fBD_SrcPath = new System.Windows.Forms.FolderBrowserDialog();
@@ -180,8 +179,8 @@
             this.Controls.Add(this.label_SrcPath);
             this.Controls.Add(this.button_SrcPath);
             this.Controls.Add(this.textBox_SrcPath);
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
+            this.ShowIcon = false;
             this.Text = "GUIWin32PrepTool";
             this.ResumeLayout(false);
             this.PerformLayout();
